aac89163622015a6b695dfbd3932d7521dc184fe
Li <lalalic@gmail.com>
2018-07-02 16:49:54 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # fetchable
 It converts data as fetchable uri  like URL.createObjectURL/revokeObjectURL
+## install
+```sh
+>npm install fetchable
+```
+
+## example
+```js
+import Fetchable from "fetchable"
+let fetchable=new Fetchable("test")
+let url=fetchable.createObjectURL([1,2,3,4])
+fetch(url)
+	.then(res=>res.arrayBuffer())
+	.then()
+fetchable.revokeObjectURL(url)
+```
---
+++ index.js
@@ -0,0 +1,43 @@
+var uuid=0
+var cached=new Map()//to support Fetchable("xxx").createObjectURL
+
+module.exports=function fetchable(PROTOCOL="blob"){
+	var cachedData=cached.get(PROTOCOL)
+	if(!cachedData){
+		cached.set(PROTOCOL,cachedData=new Map())
+	}
+
+	this.createObjectURL=function(data){
+		var url=PROTOCOL+"://"+(uuid++)
+		cachedData.set(url,{data})
+		return url
+	}
+	
+	this.revokeObjectURL=function(url){
+		return cachedData.delete(url)
+	}
+	
+	
+	//customized fetch to support `${PROTOCOL}://${id}`
+	function fetcher(url){
+		return new Promise(function(resolve, reject){
+			var cache=cachedData.get(url)
+			if(cache){
+				resolve(new Response(Buffer.from(cache.data),{}))
+			}else{
+				resolve(new Response("",{status:400, statusText:"data not exists"}))
+			}
+		})
+	}
+
+	fetcher.support=function(url){
+		if(arguments.length==0){
+			return PROTOCOL+"://"
+		}else if(url.startsWith(PROTOCOL+"://")){
+			return true
+		}
+		return false
+	}
+	
+	fetch.support(fetcher)
+}
